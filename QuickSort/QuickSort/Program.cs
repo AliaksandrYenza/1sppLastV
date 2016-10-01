48db26aa0313c01ab9ed331e532deca595351f49
AliaksandrYenza <sashayenza1996@gmail.com>
2016-10-01 16:28:09 +0300

    1
    1

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort1
+{
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            QuickSort abcd = new QuickSort();
+            Random rndm = new Random();
+
+            int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+            for (int index = 0; index < array.Length; ++index)
+                array[index] = rndm.Next(-1000, 1000);
+
+            abcd.Sort(array, 0, array.Length - 1);
+
+            for (int index = 0; index < array.Length; ++index)
+                Console.Write("{0} ", (object)array[index]);
+
+            Console.ReadKey();
+
+        }
+    }
+
+    public class QuickSort
+    {
+        private IComparer comparer = (IComparer)new ClassCMP();
+
+        public void Sort(int[] a, int left, int right)
+        {
+            int num = a[left + (right - left) / 2 + 1];
+            int i = left;
+            int j = right;
+            int temp;
+            while (i <= j)
+            {
+                while (comparer.Compare(a[i], num) == 1)
+                    ++i;
+                while (comparer.Compare(a[j], num) == -1)
+                    --j;
+                if (i <= j)
+                {
+                    Swap(ref a[i],ref a[j]);
+                   
+                    i++;
+                    j--;
+                }
+            }
+
+            if (comparer.Compare(i, right) == 1)
+                Sort(a, i, right);
+            if (comparer.Compare(left, j) == 1)
+                Sort(a, left, j);
+        }
+
+        public void Swap (ref int a, ref int b)
+        {
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+    }
+
+
+    public class ClassCMP : IComparer
+    {
+        int IComparer.Compare(object firstElement, object secondElement)
+        {
+            return new CaseInsensitiveComparer().Compare(secondElement, firstElement);
+        }
+    }
+
+}
---
+++ QuickSort/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickSort1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            QuickSort q = new QuickSort();
+            int[] array = { 1, 4, 6, 89, 66 };
+            q.Sort(array, 0, array.Length-1);
+            int[] res = { 1, 4, 6, 66, 89 };
+            CollectionAssert.AreEqual(array, res);
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            QuickSort w = new QuickSort();
+
+            int a = 6, b = 4;
+            w.Swap(ref a, ref b);
+
+            Assert.IsTrue(a == 4 && b == 6);
+        }
+    }
+}
